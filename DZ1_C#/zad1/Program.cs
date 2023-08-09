8dda63b8c524c0ae368f2b37b446c37731dab3a8
Mikhail <3x7@list.ru>
2023-08-09 19:08:50 +0300

    reshenie

--- DZ1_C#/zad1/Program.cs
+++ DZ1_C#/zad1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.Writeline ("ввод первого числа");
+    //  int a = Convert.Toint32(Console.Readline);
+//Console.Writeline ("ввод второго числа");  
+ ////     int b = Convert.Toint32(Console.Readline);//////
+ //Console.Writeline(a>b? "Первое число > второго" : "второе число > первого");
+ 
+
+
+Console.WriteLine(«Введите число»);
+int a = Convert.ToInt32(Console.ReadLine);
+Console.WriteLine(«Введите второе число»);
+int b = Convert.ToInt32(Console.ReadLine);
+if (a > b)
+System.Console.WriteLine(«Большее число: » + a + «, меньшее число: » + b);
+else System.Console.WriteLine($»Большее число: {a}, меньшее число: {b}»);
+
